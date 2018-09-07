f4b9371fea84616c0d31c99ec9a20b2820567d19
casanchez <casanchez@ROS-INF-CARINA.PJSF.GOV.AR>
2018-09-07 12:46:10 -0300

    Laboratorio 11 - Primer copia

---
+++ src/test/java/utn/frsf/of/cursojava/rrhh/web/it/TestPostCliente.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utn.frsf.of.cursojava.rrhh.web.it;
+
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ *
+ * @author casanchez
+ */
+public class TestPostCliente {
+    
+    @Test
+    public void TestPostCliente() throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode clienteJson = mapper.createObjectNode();
+        clienteJson.put("id", 1);
+        clienteJson.put("nombre", "martin");
+        clienteJson.put("correo", "mdo@mail.com");
+        clienteJson.put("cui", "203040");
+        
+        StringEntity postingString = new StringEntity(clienteJson.toString());
+        HttpPost httpPost = new HttpPost("http://localhost:8080/rrhh-web/api/cliente");
+        httpPost.setEntity(postingString);
+        httpPost.setHeader("Content-type", "application/json");
+        
+        CloseableHttpClient cliente = HttpClients.createDefault();
+        CloseableHttpResponse response1 = cliente.execute(httpPost);
+        HttpEntity entity1 = response1.getEntity();
+        String resultado;
+        resultado = entidadToString(entity1.getContent());
+        assertEquals("POST MARTIN", resultado.toUpperCase());
+        EntityUtils.consume(entity1);
+        response1.close();
+        }
+    
+        private String entidadToString(InputStream is) throws IOException {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) { 
+                sb.append(line);
+            }
+            return sb.toString();
+        }                  
+        
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
